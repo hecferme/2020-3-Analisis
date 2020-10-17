f9475d2e1cd8b3962f942c25fb48452984d023f4
HÉCTOR FERNÁNDEZ MÉNDEZ <FernandezMH@bccr.fi.cr>
2020-10-17 15:15:25 +0000

    Creación de DimProducts y proyecto vacío en VS. Segundo intento.

---
+++ transact-sql-create-table-DimProducts.sql
@@ -0,0 +1,30 @@
+USE [DWNorthWind]
+GO
+
+if ISNULL(OBJECT_ID('DimProducts', 'U'), 0) > 0
+	drop table dbo.DimProducts;
+	/****** Object:  Table [dbo].[DimProducts]    Script Date: 10/17/2020 2:58:40 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[DimProducts](
+	[ProductID] [int] NOT NULL,
+	[ProductName] [nvarchar](40) NOT NULL,
+	[CompanyName] [nvarchar](40) NOT NULL,
+	[Region] [nvarchar](15) NOT NULL,
+	[Country] [nvarchar](15) NULL,
+	[City] [nvarchar](15) NULL,
+	[CategoryName] [nvarchar](15) NOT NULL,
+	[QuantityPerUnit] [nvarchar](20) NULL,
+	[UnitPrice] [money] NULL,
+	[UnitsInStock] [smallint] NULL,
+	[UnitsOnOrder] [smallint] NULL,
+	[ReorderLevel] [smallint] NULL,
+	[Discontinued] [bit] NOT NULL
+) ON [PRIMARY]
+GO
+
+
