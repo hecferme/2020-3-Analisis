7ded5a49a19d46ab3f82f074b06b0927032ee092
HÉCTOR FERNÁNDEZ MÉNDEZ <FernandezMH@bccr.fi.cr>
2020-10-17 16:19:51 +0000

    Corrección en el cálculo de la edad y la antigüedd

--- select-create-table-DimEmployees.sql
+++ select-create-table-DimEmployees.sql
@@ -15,9 +15,9 @@ SELECT
 	e.EmployeeID, e.LastName, e.FirstName, e.Title, e.TitleOfCourtesy,
 	CONCAT(e.TitleOfCourtesy, ' ', e.FirstName, ' ', e.LastName) as EmployeeFullName,
 	e.BirthDate, 
-	DATEDIFF(MONTH, e.BirthDate, GETDATE()) / 12 as Edad,
+	FLOOR(DATEDIFF(DAY, e.BirthDate, GETDATE()) / 365.2422) as Edad,
 	e.HireDate,
-	DATEDIFF(MONTH, e.HireDate, GETDATE()) / 12 as Antiguedad,
+	FLOOR(DATEDIFF(DAY, e.HireDate, GETDATE()) / 365.2422) as Antiguedad,
 	e.City, ISNULL(e.Region, 'N/A') as Region, e.Country,
 	case isnull(e.ReportsTo, -1)
 		when -1 then 'N/A'
@@ -25,7 +25,7 @@ SELECT
 	end as ReportsToFullName,
 	'' as [EstratificacionEdad],
 	'' as [EstratificacionAntiguedad]
-INTO DWNorthWind.dbo.DimEmployees
+--INTO DWNorthWind.dbo.DimEmployees
 FROM Employees e
 	left outer join Employees e2
 		on e.ReportsTo = e2.EmployeeID;
